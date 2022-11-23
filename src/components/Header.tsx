f05679efc83b7439e842484a34cfd09c70413fbf
Juan Jacobo Jaramillo <jjjaramilloar@unal.edu.co>
2022-11-22 22:40:08 -0500

    feat: add transition to show the header smoothly when the navbar dissapear

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,50 @@
-import { Component } from "solid-js";
+import { Component, Show } from "solid-js";
 import { useParams } from "@solidjs/router";
 import { capitalize } from "../utils/words";
 import AppDarkLogo from "../assets/logo-dark.svg";
 import { useNavBarContext } from "./navbar/NavbarProvider";
+import { Motion } from "@motionone/solid";
+import { Rerun } from "@solid-primitives/keyed";
+import { classNames } from "../utils/conditionalStyles";
 
 const Header: Component = () => {
   const params = useParams();
   const [state] = useNavBarContext();
   return (
-    <div class="flex h-[6.5vh] min-h-fit w-full flex-row items-center justify-between bg-white pl-6 pr-8">
-      <div class="mt-7 mb-9 flex h-full flex-row items-center gap-8">
-        {!state.navBarDidShow ? (
-          <>
+    <Rerun on={!state.navBarDidShow}>
+      <Motion.div
+        initial={{
+          x: !state.navBarDidShow ? 90 : -10,
+          position: "absolute",
+        }}
+        animate={{
+          x: !state.navBarDidShow ? 0 : 300,
+          position: "absolute",
+        }}
+        transition={{ duration: 0.5 }}
+        class="flex h-[10.5vh] min-h-fit w-screen flex-row items-center justify-between bg-white pl-6 pr-8"
+      >
+        <div class="mt-7 mb-9 flex h-full flex-row items-center gap-8">
+          <Show when={!state.navBarDidShow}>
             <img class="h-6" src={AppDarkLogo} alt="Application Logo" />
-
             <div class="h-full w-[1px] bg-lines-light" />
-          </>
-        ) : null}
-        <h1 class="text-2xl font-bold text-light-dark">
-          {capitalize({ phrase: params.slug, sep: "-" })}
-        </h1>
-      </div>
-      <div class="flex flex-row">
-        <button>askfjkas</button>
-        <h2>aksdjfkasd</h2>
-      </div>
-    </div>
+          </Show>
+          <h1 class="text-2xl font-bold text-light-dark">
+            {capitalize({ phrase: params.slug, sep: "-" })}
+          </h1>
+        </div>
+        <div
+          class={classNames(
+            "flex flex-row",
+            state.navBarDidShow ? "mr-[300px]" : ""
+          )}
+        >
+          <button class="rounded-3xl bg-main-purple-hover py-4">
+            <p class="mx-6 font-bold text-white">+ Add New Task</p>
+          </button>
+        </div>
+      </Motion.div>
+    </Rerun>
   );
 };
 
